8c3e8690f185c3051a89792948b7013bf2f0f765
k_okulapik <okumuralapis.makarenko@gmail.com>
2025-02-03 01:30:06 +0400

    redone main window

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import pygame
 import sys
 import os
 
-run_game = False
-win_screen = False
-
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
@@ -23,118 +20,90 @@ def load_image(name, colorkey=None):
     return image
 
 
-clock = pygame.time.Clock()
-FPS = 30
-
-
 class Button:
-    def __init__(self, x, y, image, scale):
-        width = image.get_width()
-        height = image.get_height()
-        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (x, y)
-        self.clicked = False
+    def __init__(self, image, x_pos, y_pos, text_input):
+        self.image = image
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+        self.text_input = text_input
+        self.text = font.render(self.text_input, True, "white")
+        self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
 
-    def draw(self, surface):
-        action = False
-        pos = pygame.mouse.get_pos()
-        if self.rect.collidepoint(pos):
-            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
-                self.clicked = True
-                action = True
-        if pygame.mouse.get_pressed()[0] == 0:
-            self.clicked = False
-        surface.blit(self.image, (self.rect.x, self.rect.y))
-        return action
+    def update(self):
+        screen.blit(self.image, self.rect)
+        screen.blit(self.text, self.text_rect)
 
+    def checkForInput(self, position):
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top,
+                                                                                          self.rect.bottom):
+            return True
 
-def load_level(filenames):
-    res = []
-    for filename in filenames:
-        filename = "data/" + filename
-        with open(filename, 'r') as mapFile:
-            level_map = [line.strip() for line in mapFile]
-        max_width = max(map(len, level_map))
-        a = list(map(lambda x: x.ljust(max_width, '.'), level_map))
-        res += a
-    return res
+    def changeColor(self, position):
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top,
+                                                                                          self.rect.bottom):
+            self.text = font.render(self.text_input, True, "blue")
+        else:
+            self.text = font.render(self.text_input, True, "white")
 
 
-def game(screen):
+pygame.init()
+screen = pygame.display.set_mode((400, 400))
+pygame.display.set_caption('Menu')
+font = pygame.font.Font('data/monogram.ttf', 30)
+fontb = pygame.font.Font('data/monogram.ttf', 50)
+clock = pygame.time.Clock()
+
+
+def play_window():
     pass
 
 
-def start_screen(screen, w, h):
-    global win_screen, run_game
-    screen.fill('#131332')
-    intro_text = "Pacman"
-    start_button = Button(160, 200, load_image('UI-03.png'), 0.2)
-    quit_button = Button(160, 250, load_image('UI-03.png'), 0.2)
-    winners_button = Button(160, 300, load_image('UI-03.png'), 0.2)
-    font1 = pygame.font.Font('data/monogram.ttf', 50)
-    font2 = pygame.font.Font('data/monogram.ttf', 30)
-    timer = pygame.time.Clock()
-    snip = font1.render('', True, 'white')
+def winners_window():
+    pass
+
+
+def menu():
+    pygame.display.set_caption('Menu')
     counter = 0
     speed = 3
     finished = False
-    lbl1 = font2.render('Start game', True, 'white')
-    lbl2 = font2.render('quit', True, 'white')
-    lbl3 = font2.render('winners', True, 'white')
+    snip = font.render('', True, 'white')
 
-    running = True
-    while running:
-        if quit_button.draw(screen):
-            running = False
-        if start_button.draw(screen):
-            run_game = True
-        if winners_button.draw(screen):
-            winners_screen(screen)
-            running = False
-        timer.tick(60)
-        if counter < speed * len(intro_text):
+    while True:
+        screen.fill('#131332')
+        mouse_pos = pygame.mouse.get_pos()
+        title = fontb.render('Pacman', True, 'white')
+
+        clock.tick(60)
+        if counter < speed * len('Pacman'):
             counter += 1
-        elif counter >= speed * len(intro_text):
+        elif counter >= speed * len('Pacman'):
             finished = True
-        snip = font1.render(intro_text[0:counter // speed], True, 'white')
-        screen.blit(snip, (180, 100))
-        screen.blit(lbl1, (180, 205))
-        screen.blit(lbl2, (215, 250))
-        screen.blit(lbl3, (205, 305))
+        snip = fontb.render('Pacman'[0:counter // speed], True, 'white')
+        screen.blit(snip, (140, 100))
+
+        btn_pic = load_image('UI-03.png')
+        btn_pic = pygame.transform.scale(btn_pic, (150, 40))
+        play_btn = Button(btn_pic, 200, 200, 'Start game')
+        winners_btn = Button(btn_pic, 200, 250, 'Winners')
+        quit_btn = Button(btn_pic, 200, 300, 'Quit')
+
+        for btns in [play_btn, quit_btn, winners_btn]:
+            btns.changeColor(mouse_pos)
+            btns.update()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
-        pygame.display.flip()
-    return running
-
-
-def winners_screen(screen):
-    screen.fill('#131332')
-    font1 = pygame.font.Font('data/monogram.ttf', 50)
-    font1.set_italic(1)
-    lbl1 = font1.render('Best players', True, 'white')
-
-
-def main():
-    pygame.init()
-    size = w, h = 500, 500
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption('pacman')
-    if start_screen(screen, w, h):
-        running = True
-    else:
-        running = False
-    # hero, x, y = generate_level(level_map)
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        screen.fill('#6986e5')
-        clock.tick(FPS)
-        pygame.display.flip()
-    terminate()
+                terminate()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if play_btn.checkForInput(mouse_pos):
+                    play_window()
+                if winners_btn.checkForInput(mouse_pos):
+                    winners_window()
+                if quit_btn.checkForInput(mouse_pos):
+                    terminate()
+        pygame.display.update()
 
 
 def terminate():
@@ -143,4 +112,4 @@ def terminate():
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    sys.exit(menu())
